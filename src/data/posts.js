a9b12d2aae9657a85da083d5bf5fe4c63071bf62
Melrizzuto <melrizzuto4@gmail.com>
2024-12-13 15:21:18 +0100

    added more posts js

--- src/data/posts.js
+++ src/data/posts.js
@@ -39,6 +39,22 @@ const posts = [
         image: "",
         tags: ["trip", "landscape", "lowcost"],
         published: true,
+    },
+    {
+        id: 6,
+        title: "Danimarca",
+        content: "Scopri la magia dei paesaggi danesi, tra coste mozzafiato, colline verdi e architettura affascinante.",
+        image: "/danimarca.jpg",
+        tags: ["trip", "landscape", "expansive"],
+        published: true,
+    },
+    {
+        id: 7,
+        title: "Copenhagen",
+        content: "La capitale della Danimarca, famosa per i suoi canali, il parco divertimenti Tivoli e la statua della Sirenetta.",
+        image: "/copenhagen.jpg",
+        tags: ["trip", "landscape", "expansive"],
+        published: true,
     }
 ];
 
